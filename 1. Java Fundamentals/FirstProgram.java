63b3c1071a702b07470ec2f55dbb911bd7b5ca7e
Arun-0190 <158183704+Arun-0190@users.noreply.github.com>
2024-11-25 15:44:37 +0530

    First Commit

---
+++ 1. Java Fundamentals/FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        //public - access modifier
+        //static - keyword that allows main to be called before an object of the class
+        //void - is a return type
+
+        System.out.println("Hello World!!");
+
+    }
+}
