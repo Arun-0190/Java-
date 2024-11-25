ac2176a87dc1ed3c7591c18dbf22d7f1e7f0c8ed
Arun-0190 <158183704+Arun-0190@users.noreply.github.com>
2024-11-25 15:58:20 +0530

    Second test commit

---
+++ 1. Java Fundamentals/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int var;           //this declares a variable
+
+        var = 100;          //this assigns 100 to var
+        System.out.println("My variable has: "+ var);           //can be like this
+        //or
+        System.out.print("My variable has: ");
+        System.out.println(var);                //can be like this too
+    }
+
+}
