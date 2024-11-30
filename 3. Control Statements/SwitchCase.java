8ede2272a5b5e92a5640c972bb54ffac63cbaf6f
Arun-0190 <158183704+Arun-0190@users.noreply.github.com>
2024-12-01 04:55:38 +0530

    Commit

--- 3. Control Statements/SwitchCase.java
+++ 3. Control Statements/SwitchCase.java
@@ -2,12 +2,43 @@ import java.io.IOException;
 
 public class SwitchCase {
 
-    //Simple Calculator using Switch case statements
+    //Identifying different special characters using Switch case statements
     public static void main(String[] args) throws IOException {
 
-        System.out.print("Enter the operation you wanna perform (+,-,*,%,/): ");
+        System.out.print("Enter the special character: ");
         char sign = (char) System.in.read();
 
+        switch(sign){
+
+            //case 1
+            case '!':
+                System.out.println("It's an exclamation mark.");
+                break;
+
+            //case 2
+            case '@':
+                System.out.println("It's an at sign.");
+                break;
+
+            //case 3
+            case '#':
+                System.out.println("It's a hash sign.");
+                break;
+
+            //case 4
+            case '$':
+                System.out.println("It's a dollar sign.");
+                break;
+
+            //case 5
+            case '&':
+                System.out.println("It's a ampersand sign.");
+                break;
+
+            //default case
+            default:
+                System.out.println("Any sign did not matched.");
+        }
 
     }
 }
