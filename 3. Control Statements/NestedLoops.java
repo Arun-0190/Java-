e6729f7aa98a169da1b5a61e171dedac451b498a
Arun-0190 <158183704+Arun-0190@users.noreply.github.com>
2024-12-04 21:23:02 +0530

    Commit

---
+++ 3. Control Statements/NestedLoops.java
@@ -0,0 +1,23 @@
+public class NestedLoops {
+
+    //program to demonstrate the nested loops finding the factors of numbers from 2 to 100
+    public static void main(String[] args) {
+
+        for(int i = 2; i <= 100; i++){
+
+            System.out.print("Factors of "+i+": ");
+
+            //inner loop
+            for(int j = 2; j < i; j++){
+
+                //checking the condition
+                if(i%j == 0){
+                    System.out.print(j+" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+}
