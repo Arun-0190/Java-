1493d2268b48b593afc5409567b39a767ec2a749
Arun-0190 <158183704+Arun-0190@users.noreply.github.com>
2024-12-13 19:23:07 +0530

    Commit

--- 004. Classes, Objects and Methods/ClassDemo.java
+++ 004. Classes, Objects and Methods/ClassDemo.java
@@ -30,8 +30,25 @@ class ClassDemo {
         student2.department = "Technology";
 
         //printing the first details
-        System.out.println("Name: "+student1.name+"\n"+"Roll Num: "+student1.RollNum+"\n"+"Department: "+student1.department);
-        System.out.println("\n"+"Name: "+student2.name+"\n"+"Roll Num: "+student2.RollNum+"\n"+"Department: "+student2.department);
+        System.out.println("\nName: "+student1.name+"\n"+"Roll Num: "+student1.RollNum+"\n"+"Department: "+student1.department);
+        System.out.println("\nName: "+student2.name+"\n"+"Roll Num: "+student2.RollNum+"\n"+"Department: "+student2.department);
+
+
+        //understanding the concept of reference variables and assignment
+        FirstClass student3 = new FirstClass();
+        FirstClass student4 = student3;         //now student 3 and 4 refers to same object
+        FirstClass student5 = new FirstClass();
+        student5 = student4;                        //4 and 5 refers to same object
+
+        student3.name = "Shyam";
+        student3.RollNum = 18;
+        student3.department = "Electrical";
+
+        System.out.println("\nName: "+student4.name+"\n"+"Roll Num: "+student4.RollNum+"\n"+"Department: "+student4.department);
+        //even when the elements of student 4 were not directly provided but, we can access it directly
+
+        System.out.println("\nName: "+student5.name+"\n"+"Roll Num: "+student5.RollNum+"\n"+"Department: "+student5.department);
+
 
     }
 
