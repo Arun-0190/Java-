68816e6b4d7054573ef626f5694e9ec917cab483
Arun-0190 <arunishere2003@gmail.com>
2024-12-23 16:09:44 +0530

    Commit

---
+++ 005. More Data Structures & Operators/StringsDemo.java
@@ -0,0 +1,16 @@
+public class StringsDemo {
+
+    public static void main(String[] args) {
+
+        //ways to declare a string
+        String str = new String("Hello Bro.");
+        String str2 = new String(str);
+        String str3 = "Hii Brother";
+
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(str3);
+
+    }
+
+}
