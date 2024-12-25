d930fe11b75b227906661ed89accc56f702463dc
Arun-0190 <arunishere2003@gmail.com>
2024-12-25 14:05:24 +0530

    Commit

---
+++ 005. More Data Structures & Operators/StringArray.java
@@ -0,0 +1,15 @@
+public class StringArray {
+
+    public static void main(String[] args) {
+
+        //array of strings
+        String[] str = {"Arun", "Farida", "Lakhan", "Ram"};
+
+        //printing
+        for(String s : str){
+            System.out.println(s);
+        }
+
+    }
+
+}
