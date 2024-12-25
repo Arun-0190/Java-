46b8698b8b2a8a171c39aca477e290dfd93baeba
Arun-0190 <arunishere2003@gmail.com>
2024-12-25 14:22:41 +0530

    Commit

---
+++ 005. More Data Structures & Operators/Substr.java
@@ -0,0 +1,14 @@
+public class Substr {
+    //using substring method for string manipulation
+
+    public static void main(String[] args) {
+
+        String original = "My name is Arun";
+
+        //constructing a substring
+        String substr = original.substring(11, 15);
+        System.out.println("Original: "+ original);
+        System.out.println("Substring: "+substr);
+
+    }
+}
