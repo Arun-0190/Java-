cb4e15b71e90fc4ae12a91750662ef5545d4a74a
Arun-0190 <arunishere2003@gmail.com>
2025-04-02 00:27:08 +0530

    Commit

--- 006. Methods and Classes/ConstructorOverloading.java
+++ 006. Methods and Classes/ConstructorOverloading.java
@@ -26,8 +26,10 @@ class OverConst{
         x = i*j;
     }
 
-    OverConst(int i, int j, int k){
+    OverConst(OverConst ov){                    //constructed one object from another
 
+        System.out.println("Inside the fourth constructor.");
+        x = ov.x;
     }
 
 }
@@ -40,11 +42,13 @@ public class ConstructorOverloading {
         OverConst ov2 = new OverConst(5);
         OverConst ov3 = new OverConst(56.9);
         OverConst ov4 = new OverConst(4,6);
+        OverConst ov5 = new OverConst(ov3);
 
         System.out.println("ov1.x: "+ov1.x);
         System.out.println("ov2.x: "+ov2.x);
         System.out.println("ov3.x: "+ov3.x);
         System.out.println("ov4.x: "+ov4.x);
+        System.out.println("ov5.x: "+ov5.x);
 
     }
 
